1a94074b1f021d32a62face64c3b8f193fdd6fb8
Andy Rich <arich@microsoft.com>
2020-12-01 22:13:24 -0800

    Removed 2019, added solns for first two 2020s

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -15,11 +18,40 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
+            string[] lines = Input.SplitByNewline();
+            var values = lines.Select(x => int.Parse(x)).ToImmutableSortedSet();
+            
+            for (int i = 0; i < values.Count; i++)
+            {
+                int target = 2020 - values[i];
+                if (values.Contains(target))
+                {
+                    return (target * values[i]).ToString();
+                }
+            }
+
             return null;
         }
 
         protected override string SolvePartTwo()
         {
+            string[] lines = Input.SplitByNewline();
+            var values = lines.Select(x => int.Parse(x)).ToImmutableSortedSet();
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                for (int j = i+1; j < values.Count; j++)
+                {
+                    int target = 2020 - (values[i] + values[j]);
+                    {
+                        if (values.Contains(target) && values.IndexOf(target) != i && values.IndexOf(target) != j)
+                        {
+                            return (target * values[i] * values[j]).ToString();
+                        }
+                    } 
+                }
+            }
+
             return null;
         }
     }
--- AdventOfCode/Solutions/Year2020/Day02/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day02/Solution.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -15,12 +17,50 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
-            return null;
+            string[] lines = Input.SplitByNewline();
+            int validPasses = 0;
+
+            foreach(var line in lines)
+            {
+                var parts = line.Split(':');
+                var conditions = parts[0].Split(' ');
+                var conditionValue = conditions[1].ElementAt(0);
+                var conditionNums = conditions[0].Split('-').Select(x => int.Parse(x)).ToArray();
+                var password = parts[1];
+
+                int count = password.Count(c => c == conditionValue);
+                if (count >= conditionNums[0] && count <= conditionNums[1])
+                {
+                    validPasses++;
+                }
+            }
+
+            return validPasses.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            string[] lines = Input.SplitByNewline();
+            int validPasses = 0;
+
+            foreach (var line in lines)
+            {
+                var parts = line.Split(':');
+                var conditions = parts[0].Split(' ');
+                var conditionValue = conditions[1].ElementAt(0);
+                var conditionNums = conditions[0].Split('-').Select(x => int.Parse(x)).ToArray();
+                var password = parts[1].Trim();
+
+                var amatch = password[conditionNums[0] - 1] == conditionValue;
+                var bmatch = password[conditionNums[1] - 1] == conditionValue;
+
+                if (amatch != bmatch)
+                {
+                    validPasses++;
+                }
+            }
+
+            return validPasses.ToString();
         }
     }
 }
