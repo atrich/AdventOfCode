085b9d4c8da1e21e6877211bb8e7fcfbc7919023
Andy Rich <arich@microsoft.com>
2020-12-02 21:22:06 -0800

    Solution 3

--- AdventOfCode/Solutions/Year2020/Day03/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day03/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -10,17 +11,39 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day03() : base(03, 2020, "")
         {
+        }
 
+        private int CheckSlope(int x, int y)
+        {
+            string[] lines = Input.SplitByNewline();
+            int width = lines[0].Length;
+            int depth = lines.Length;
+            int xpos = 0, ypos = 0;
+            int trees = 0;
+
+            while (ypos < depth - 1)
+            {
+                ypos += y;
+                xpos += x;
+                if (xpos >= width) xpos -= width;
+                if (lines[ypos][xpos] == '#')
+                {
+                    trees++;
+                }
+            }
+
+            return trees;
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return CheckSlope(3, 1).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int x = CheckSlope(1, 1) * CheckSlope(3, 1) * CheckSlope(5, 1) * CheckSlope(7, 1) * CheckSlope(1, 2);
+            return x.ToString();
         }
     }
 }
