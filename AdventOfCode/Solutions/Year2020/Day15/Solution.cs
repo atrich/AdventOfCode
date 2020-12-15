2f1e6d9aa50fb0f300f3d2ad736b5f869f9bedf2
Andy Rich <arich@microsoft.com>
2020-12-14 22:11:31 -0800

    Day 15

--- AdventOfCode/Solutions/Year2020/Day15/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day15/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -10,17 +11,57 @@ namespace AdventOfCode.Solutions.Year2020
 
         public Day15() : base(15, 2020, "")
         {
+            // DebugInput = "0,3,6";
+        }
 
+        ulong playRounds(string input, ulong lastRound)
+        {
+            var nums = Input.Split(',').Select(int.Parse);
+            var counts = new Dictionary<ulong, ulong>();
+            var round = 0ul;
+
+            foreach (var n in nums)
+            {
+                counts[(ulong)n] = round++;
+            }
+
+            var num = (ulong)nums.Last();
+            var prevdiff = 0ul;
+
+            while (round < lastRound)
+            {
+                if (prevdiff == 0)
+                {
+                    num = 0;
+                }
+                else
+                {
+                    num = prevdiff;
+                }
+
+                if (counts.ContainsKey(num))
+                {
+                    prevdiff = round - counts[num];
+                }
+                else
+                {
+                    prevdiff = 0;
+                }
+
+                counts[num] = round++;
+            }
+
+            return num;
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            return playRounds(Input, 2020).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            return playRounds(Input, 30000000).ToString();
         }
     }
 }
