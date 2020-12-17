a0eb886d458cacf4e476ee38de895c1bb65bf40c
Andy Rich <arich@microsoft.com>
2020-12-17 00:49:56 -0800

    Days 16 and 17

--- AdventOfCode/Solutions/Year2020/Day16/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day16/Solution.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -13,14 +15,133 @@ namespace AdventOfCode.Solutions.Year2020
 
         }
 
+        public Tuple<int, string[]> ProcessTickets(string[] tickets, Dictionary<string, Tuple<int, int>[]> ruleset)
+        {
+            int err = 0;
+            var validtickets = new List<string>();
+
+            foreach (var ticket in tickets)
+            {
+                var values = ticket.Split(',').Select(int.Parse);
+                var anyerr = false;
+                foreach (var value in values)
+                {
+                    var found = false;
+
+                    foreach (var rule in ruleset)
+                    {
+                        foreach (var range in rule.Value)
+                        {
+                            if (value >= range.Item1 && value <= range.Item2)
+                            {
+                                found = true;
+                                break;
+                            }
+                        }
+
+                        if (found == true) break;
+                    }
+
+                    if (!found)
+                    {
+                        err += value;
+                        anyerr = true;
+                    }
+                }
+
+                if (!anyerr)
+                {
+                    validtickets.Add(ticket);
+                }
+            }
+
+            return Tuple.Create(err, validtickets.ToArray());
+        }
+
+        public Dictionary<string, Tuple<int, int>[]> CreateRuleset(string[] rules)
+        {
+            var ruleset = new Dictionary<string, Tuple<int, int>[]>(); // maps a field to the set of ranges
+            foreach (var rule in rules)
+            {
+                var name = rule.Substring(0, rule.IndexOf(':'));
+                ruleset[name] = rule
+                    .Substring(rule.IndexOf(':') + 1)
+                    .Split("or", 2, StringSplitOptions.TrimEntries)
+                    .Select(x => x.Split('-').Select(int.Parse).ToArray())
+                    .Select(x => Tuple.Create(x[0], x[1]))
+                    .ToArray();
+            }
+
+            return ruleset;
+        }
+
         protected override string SolvePartOne()
         {
-            return null;
+            var lines = Input.SplitByNewline();
+            var rules = lines.AsSpan(0, Array.IndexOf(lines, "your ticket:")).ToArray();
+            var tickets = lines.AsSpan(Array.IndexOf(lines, "nearby tickets:") + 1).ToArray();
+            var ruleset = CreateRuleset(rules);
+            var res = ProcessTickets(tickets, ruleset);
+            return res.Item1.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var lines = Input.SplitByNewline();
+            var rules = lines.AsSpan(0, Array.IndexOf(lines, "your ticket:")).ToArray();
+            var myticket = lines[Array.IndexOf(lines, "your ticket:") + 1].Split(',').Select(int.Parse).ToArray();
+            var tickets = lines.AsSpan(Array.IndexOf(lines, "nearby tickets:") + 1).ToArray();
+            var ruleset = CreateRuleset(rules);
+            var validtix = ProcessTickets(tickets, ruleset).Item2.Select(x => x.Split(',').Select(int.Parse).ToArray()).ToArray();
+            var indexmap = new Dictionary<string, int[]>();
+
+            foreach(var rule in ruleset)
+            {
+                var possibles = Enumerable.Repeat(true, validtix[0].Length).ToArray();
+                int i = 0;
+                while(possibles.Count(x => x == true) > 1 && i < possibles.Length)
+                {
+                    foreach(var ticket in validtix)
+                    {
+                        bool found = false;
+                        foreach (var range in rule.Value)
+                        {
+                            var value = ticket[i];
+                            if (value >= range.Item1 && value <= range.Item2)
+                            {
+                                found = true;
+                                break;
+                            }
+                        }
+
+                        if (!found)
+                        {
+                            possibles[i] = false;
+                            break;
+                        }
+                    }
+
+                    i++;
+                }
+
+                indexmap[rule.Key] = possibles.Select((x, i) => x ? i : -1).Where(x => x != -1).ToArray();
+            }
+
+            var finalmap = new Dictionary<string, int>();
+            IEnumerable<KeyValuePair<string, int[]>> m = indexmap;
+
+            while(finalmap.Keys.Count < m.Count())
+            {
+                var uniq = m.First(x => x.Value.Length == 1);
+                var v = uniq.Value.First();
+                finalmap[uniq.Key] = v;
+
+                // remove all v entries
+                m = m.Select(x => KeyValuePair.Create(x.Key, x.Value.Where(i => i != v).ToArray()));
+            }
+
+            var total = finalmap.Where(x => x.Key.StartsWith("departure")).Select(x => (ulong)myticket[x.Value]).Aggregate(1ul, (x, y) => x * y);
+            return total.ToString();
         }
     }
 }
--- AdventOfCode/Solutions/Year2020/Day17/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day17/Solution.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,20 +11,183 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day17 : ASolution
     {
-
         public Day17() : base(17, 2020, "")
         {
-
+            //DebugInput = ".#.\n..#\n###\n";
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var lines = Input.SplitByNewline();
+            var map = new SortedSet<Tuple<int, int, int>>();
+            int y = 0;
+
+            foreach(var line in lines)
+            {
+                for(int x = 0; x < line.Length; x++)
+                {
+                    if (line[x] == '#') map.Add(Tuple.Create(0, x, y));
+                }
+                y++;
+            }
+
+            int minz = -1;
+            int maxz = 1;
+            int minx = -1;
+            int maxx = lines[0].Length;
+            int miny = -1;
+            int maxy = lines.Length;
+
+            for(int i = 0; i < 6; i ++)
+            {
+                var nextmap = new SortedSet<Tuple<int, int, int>>();
+
+                for (int z = minz; z <= maxz; z++)
+                {
+                    for(y = miny; y <= maxy; y++)
+                    {
+                        for(int x = minx; x <= maxx; x++)
+                        {
+                            int nbr = 0;
+                            for(int dx = x-1; dx <= x+1; dx++)
+                            {
+                                for(int dy = y-1; dy <= y+1; dy++)
+                                {
+                                    for(int dz = z-1; dz <= z+1; dz++)
+                                    {
+                                        if (x == dx && y == dy && z == dz) continue;
+                                        if(map.Any(p => p.Item1 == dz && p.Item2 == dx && p.Item3 == dy))
+                                        {
+                                            nbr++;
+                                        }
+                                    }
+                                }
+                            }
+
+                            if(map.Any(p => p.Item1 == z && p.Item2 == x && p.Item3 == y))
+                            {
+                                if (nbr == 2 || nbr == 3)
+                                {
+                                    nextmap.Add(Tuple.Create(z, x, y));
+                                }
+                            }
+                            else
+                            {
+                                if (nbr == 3)
+                                {
+                                    nextmap.Add(Tuple.Create(z, x, y));
+                                }
+                            }
+                        }
+                    }
+                }
+
+                map = nextmap;
+                maxx++;
+                maxy++;
+                maxz++;
+                minx--;
+                miny--;
+                minz--;
+            }
+
+            return map.Count.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var lines = Input.SplitByNewline();
+            var map = new Dictionary<int, Dictionary<int, Dictionary<int, List<int>>>>();
+            int y = 0;
+
+            map[0] = new Dictionary<int, Dictionary<int, List<int>>>();
+            map[0][0] = new Dictionary<int, List<int>>();
+
+            foreach (var line in lines)
+            {
+                map[0][0][y] = new List<int>();
+                for (int x = 0; x < line.Length; x++)
+                {
+                    if (line[x] == '#') map[0][0][y].Add(x);
+                }
+                y++;
+            }
+
+            int minw = -1;
+            int maxw = 1;
+            int minz = -1;
+            int maxz = 1;
+            int minx = -1;
+            int maxx = lines[0].Length;
+            int miny = -1;
+            int maxy = lines.Length;
+
+            for (int i = 0; i < 6; i++)
+            {
+                var nextmap = new Dictionary<int, Dictionary<int, Dictionary<int, List<int>>>>();
+
+                for (int w = minw; w <= maxw; w++)
+                {
+                    nextmap[w] = new Dictionary<int, Dictionary<int, List<int>>>();
+
+                    for (int z = minz; z <= maxz; z++)
+                    {
+                        nextmap[w][z] = new Dictionary<int, List<int>>();
+                        for (y = miny; y <= maxy; y++)
+                        {
+                            nextmap[w][z][y] = new List<int>();
+                            for (int x = minx; x <= maxx; x++)
+                            {
+                                int nbr = 0;
+                                for (int dx = x - 1; dx <= x + 1; dx++)
+                                {
+                                    for (int dy = y - 1; dy <= y + 1; dy++)
+                                    {
+                                        for (int dz = z - 1; dz <= z + 1; dz++)
+                                        {
+                                            for (int dw = w - 1; dw <= w + 1; dw++)
+                                            {
+                                                if (x == dx && y == dy && z == dz && w == dw) continue;
+                                                if (map.ContainsKey(dw) && map[dw].ContainsKey(dz) && map[dw][dz].ContainsKey(dy) && map[dw][dz][dy].Contains(dx))
+                                                {
+                                                    nbr++;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                                if (map.ContainsKey(w) && map[w].ContainsKey(z) && map[w][z].ContainsKey(y) && map[w][z][y].Contains(x))
+                                {
+                                    if (nbr == 2 || nbr == 3)
+                                    {
+                                        nextmap[w][z][y].Add(x);
+                                    }
+                                }
+                                else
+                                {
+                                    if (nbr == 3)
+                                    {
+                                        nextmap[w][z][y].Add(x);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                map = nextmap;
+                maxx++;
+                maxy++;
+                maxz++;
+                maxw++;
+                minx--;
+                miny--;
+                minz--;
+                minw--;
+            }
+
+            return map.Aggregate(0, (s, w) => s + w.Value.Aggregate(0, (s, z) => s + z.Value.Aggregate(0, (s, y) => s + y.Value.Count))).ToString();
         }
     }
 }
