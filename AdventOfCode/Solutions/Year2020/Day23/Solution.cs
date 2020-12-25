a9fd13566b26efcb0059fac77e17e9c6909d1ddd
Andy Rich <arich@microsoft.com>
2020-12-24 16:52:02 -0800

    Days 23 and 24

--- AdventOfCode/Solutions/Year2020/Day23/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day23/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,20 +8,120 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day23 : ASolution
     {
+        class Card
+        {
+            public int val;
+            public int next;
+        }
 
         public Day23() : base(23, 2020, "")
         {
+            // DebugInput = "389125467";
+        }
 
+        int WrapInc<T>(int i, List<T> coll)
+        {
+            i++;
+            if (i >= coll.Count) return 0;
+            return i;
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            var deck = Input.ToCharArray().Select(x => int.Parse(x.ToString())).ToList();
+            var cur = 0;
+
+            for(int t = 0; t < 100; t++)
+            {
+                var curc = deck[cur];
+                var hand = new List<int>();
+                var pick = WrapInc(cur, deck);
+                for (int i = 0; i < 3; i++)
+                {
+                    hand.Add(deck[pick]);
+                    deck.RemoveAt(pick);
+                    if (pick >= deck.Count) pick = 0;
+                }
+
+                var x = curc - 1;
+                if (x == 0) x = 9;
+                while (hand.Contains(x))
+                {
+                    x--;
+                    if (x == 0) x = 9;
+                }
+
+                deck.InsertRange(deck.IndexOf(x) + 1, hand);
+                cur = WrapInc(deck.IndexOf(curc), deck);
+            }
+
+            var p = WrapInc(deck.IndexOf(1), deck);
+
+            var sb = new StringBuilder();
+            do
+            {
+                sb.Append(deck[p]);
+                p = WrapInc(p, deck);
+            } while (p != deck.IndexOf(1));
+
+            return sb.ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            var inp = Input.ToCharArray().Select(x => int.Parse(x.ToString())).ToList();
+            var deck = new Card[1000000];
+            var dl = new Dictionary<int, Card>();
+
+            for(int i = 0; i < inp.Count; i++)
+            {
+                deck[i] = new Card { val = inp[i], next = i + 1 };
+                dl[inp[i]] = deck[i];
+            }
+
+            for(int i = 9; i < 1000000; i++)
+            {
+                deck[i] = new Card { val = i + 1, next = i + 1 };
+                dl[i + 1] = deck[i];
+            }
+
+            deck[999999].next = 0;
+
+            var curc = deck[0];
+            var max = deck.Length;
+
+            for (long t = 0; t < 10000000; t++)
+            {
+                var sh = curc.next;
+                var eh = deck[deck[deck[sh].next].next];
+                curc.next = eh.next;
+
+                var fv = curc.val - 1;
+                if (fv < 1) fv = max;
+                 
+                while(deck[sh].val == fv || deck[deck[sh].next].val == fv || deck[deck[deck[sh].next].next].val == fv)
+                {
+                    fv--;
+                    if (fv < 1) fv = max;
+                }
+
+                var ip = dl[fv];
+
+                eh.next = ip.next;
+                ip.next = sh;
+                curc = deck[curc.next];
+            }
+
+            var p = dl[1];
+            long tot = 1;
+
+            for(int i = 0; i < 2; i++)
+            {
+                p = deck[p.next];
+                tot *= p.val;
+            }
+
+            return tot.ToString();
         }
     }
 }
--- AdventOfCode/Solutions/Year2020/Day24/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day24/Solution.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.Solutions.Year2020
@@ -7,20 +8,128 @@ namespace AdventOfCode.Solutions.Year2020
 
     class Day24 : ASolution
     {
+        Dictionary<int, Dictionary<int, bool>> map;
+        int minx = 0, miny = 0, maxx = 0, maxy = 0;
 
         public Day24() : base(24, 2020, "")
         {
+            //DebugInput = "sesenwnenenewseeswwswswwnenewsewsw\nneeenesenwnwwswnenewnwwsewnenwseswesw\nseswneswswsenwwnwse\nnwnwneseeswswnenewneswwnewseswneseene\nswweswneswnenwsewnwneneseenw\neesenwseswswnenwswnwnwsewwnwsene\nsewnenenenesenwsewnenwwwse\nwenwwweseeeweswwwnwwe\nwsweesenenewnwwnwsenewsenwwsesesenwne\nneeswseenwwswnwswswnw\nnenwswwsewswnenenewsenwsenwnesesenew\nenewnwewneswsewnwswenweswnenwsenwsw\nsweneswneswneneenwnewenewwneswswnese\nswwesenesewenwneswnwwneseswwne\nenesenwswwswneneswsenwnewswseenwsese\nwnwnesenesenenwwnenwsewesewsesesew\nnenewswnwewswnenesenwnesewesw\neneswnwswnwsenenwnwnwwseeswneewsenese\nneswnwewnwnwseenwseesewsenwsweewe\nwseweeenwnesenwwwswnew";
+        }
 
+        void BuildMap()
+        {
+            map = new Dictionary<int, Dictionary<int, bool>>();
+            var lines = Input.SplitByNewline();
+
+            foreach (var line in lines)
+            {
+                int i = 0, x = 0, y = 0;
+                char prev = ' ';
+                while (i < line.Length)
+                {
+                    switch (line[i])
+                    {
+                        case 'n':
+                            y--;
+                            break;
+
+                        case 's':
+                            y++;
+                            break;
+
+                        case 'e':
+                            if (prev != 'n')
+                            {
+                                x++;
+                            }
+                            break;
+
+                        case 'w':
+                            if (prev != 's')
+                            {
+                                x--;
+                            }
+                            break;
+                    }
+                    prev = line[i++];
+                }
+
+                Flip(map, x, y);
+                minx = Math.Min(x, minx);
+                miny = Math.Min(y, miny);
+                maxx = Math.Max(x, maxx);
+                maxy = Math.Max(y, maxy);
+            }
         }
 
         protected override string SolvePartOne()
         {
-            return null;
+            BuildMap();
+            return map.SelectMany(x => x.Value.Where(y => y.Value)).Count().ToString();
+        }
+
+        bool Chk(int x, int y)
+        {
+            return map.ContainsKey(x) && map[x].ContainsKey(y) && map[x][y];
+        }
+
+        void Flip(Dictionary<int, Dictionary<int, bool>> n, int x, int y)
+        {
+            if (!n.ContainsKey(x)) n[x] = new Dictionary<int, bool>();
+            if (!n[x].ContainsKey(y)) n[x][y] = true;
+            else n[x][y] = !n[x][y];
+        }
+
+        Dictionary<int, Dictionary<int, bool>> Clone(Dictionary<int, Dictionary<int, bool>> m)
+        {
+            var n = new Dictionary<int, Dictionary<int, bool>>();
+            foreach (var dx in m)
+            {
+                var p = new Dictionary<int, bool>();
+                foreach (var dy in dx.Value) p[dy.Key] = dy.Value;
+                n[dx.Key] = p;
+            }
+            return n;
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            BuildMap();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var n = Clone(map);
+
+                for (int x = minx - 1; x <= maxx + 1; x++)
+                    for (int y = miny - 1; y <= maxy + 1; y++)
+                    {
+                        var blk = 0;
+                        if (Chk(x, y - 1)) blk++; // NE
+                        if (Chk(x + 1, y)) blk++; // E
+                        if (Chk(x + 1, y + 1)) blk++; // SE
+                        if (Chk(x, y + 1)) blk++; // SW
+                        if (Chk(x - 1, y)) blk++; // W
+                        if (Chk(x - 1, y - 1)) blk++; // NW
+
+                        var t = Chk(x, y);
+                        if (t && (blk == 0 || blk > 2))
+                        {
+                            Flip(n, x, y);
+                        }
+                        else if (!t && blk == 2)
+                        {
+                            Flip(n, x, y);
+                        }
+                    }
+
+                map = n;
+                minx--;
+                miny--;
+                maxx++;
+                maxy++;
+            }
+
+            return map.SelectMany(x => x.Value.Where(y => y.Value)).Count().ToString();
         }
     }
 }
